bfb6335a3d3273098fd2c00c32b69cfa72102025
James Evans <jim.p.evans@outlook.com>
2025-05-27 09:16:59 -0400

    Add project files.

---
+++ ARK.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ARK.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to ARK Core API!");
+        }
+
+        [HttpGet("health")]
+        public IActionResult HealthCheck()
+        {
+            return Ok("API is healthy!");
+        }
+
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            return Ok(new { Status = "Running", Timestamp = DateTime.UtcNow });
+        }
+    }
+}
