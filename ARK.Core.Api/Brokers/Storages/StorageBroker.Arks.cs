f1beb8efa066fe43fbb89e24075f8fa5eec7ba09
James Evans <jim.p.evans@outlook.com>
2025-05-27 15:36:08 -0400

    Implement data access

---
+++ ARK.Core.Api/Brokers/Storages/StorageBroker.Arks.cs
@@ -0,0 +1,15 @@
+﻿using ARK.Core.Api.Models.ARKS;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARK.Core.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Ark> Arks { get; set; }
+
+        public async ValueTask<IQueryable<Ark>> SelectAllArksAsync() =>
+            await this.SelectAll<Ark>();
+    }
+}
---
+++ ARK.Core.Api/Migrations/20250527192415_add-ark-model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARK.Core.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class addarkmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Arks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Act = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Arks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Arks");
+        }
+    }
+}
